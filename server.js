22f219d4ed4988378728ee88736e866e51ca6491
Rita Rangel <rita.d.rangel@accenture.com>
2023-10-25 12:41:41 -0300

    fix: angular servser.js port

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.static(__dirname + `/dist/${appName}`));
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + `/dist/${appName}/index.html`))
 })
-app.listen(8080)
+app.listen(process.env.PORT || 5000)
